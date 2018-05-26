6d386dc9bb7ea49681f9993cc610426338e6b2e0
Daniel Marantz <dmarantz@dmarantz-mac01.local>
2018-05-26 15:48:17 -0700

    base presenter layer architecture

---
+++ app/src/main/java/com/flickr/assessment/flickrassessment/presenter/base/BasePresenter.java
@@ -0,0 +1,9 @@
+package com.flickr.assessment.flickrassessment.presenter.base;
+
+/**
+ * setPanel() will be called to pass view reference to presenter
+ */
+public abstract class BasePresenter<P> {
+
+    public abstract void setPanel(P panel);
+}
